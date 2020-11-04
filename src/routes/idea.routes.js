1255d77a20188251f1b4e7714334b4bf64fa5dfb
4rturd13 <4rturd13@gmail.com>
2020-11-04 15:48:49 -0500

    [ADD] authentication implement

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -6,6 +6,7 @@ module.exports = function ({ IdeaController }) {
   router.get('', IdeaController.getAll)
   router.get('/:ideaId', IdeaController.get)
   router.get('/:userId/all', IdeaController.getUserIdeas)
+  router.post('', IdeaController.create)
   router.patch('/:ideaId', IdeaController.update)
   router.delete('/:ideaId', IdeaController.delete)
   router.post(':ideaId/upvote', IdeaController.upvoteIdea)
